cb0c7cb4814c58e646a55f2474b0430766e0e246
amri <amriroxadi@gmail.com>
2022-02-17 20:36:16 +0800

    arsitacture of nodeJs and what is nodeJS

---
+++ exa-sync-async.js
@@ -0,0 +1,27 @@
+// Synchronous
+
+const getUserSync = require("./src/getUserSync");
+
+const firstUser = getUserSync(1);
+console.log(firstUser);
+
+const secondUser = getUserSync(2);
+console.log(secondUser);
+
+const halo = "Hello World";
+console.log(halo);
+
+// Asynchronous
+
+const getUser = require("./src/getUser");
+
+const firstUser = getUser(1, (user) => {
+  console.log(user);
+});
+
+const secondUser = getUser(2, (user) => {
+  console.log(user);
+});
+
+const halo = "Hello World";
+console.log(halo);
