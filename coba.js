ec4b5bb47fd307a7d45e48f166cff331a423d2fd
amri <amriroxadi@gmail.com>
2022-02-18 17:10:08 +0800

    how to execute a file and how to execute a file in the other file by exports

---
+++ coba.js
@@ -0,0 +1,7 @@
+// console.log("Hello World!");
+
+function printName(name) {
+  return `Hi, my name is ${name}`;
+}
+
+module.exports = printName;
---
+++ index.js
@@ -0,0 +1,11 @@
+// const name = "Amri Rosadi";
+
+// console.log(name);
+
+// const printName = (name) => `Hi, my name is ${name}`;
+// console.log(printName("Umar dan Umair"));
+
+const printName = require("./coba");
+
+// console.log("Hello ubay!");
+console.log(printName("Amri"));
