fc7aa38c31d7cb6d00b7850f92e974439178c122
amri <amriroxadi@gmail.com>
2022-02-19 09:39:08 +0800

    simple contacts API with 2 core module, Readline and fs module

--- 7-node-core-module/app.js
+++ 7-node-core-module/app.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 // // write string to file syncrronously
 // try {
-//   fs.writeFileSync("data/test.txt", "Hello world secara synchrounous");
+//   fs.writeFileSync("data/contacts.json", "Hello world secara synchrounous");
 // } catch (e) {
 //   console.log(e);
 // }
@@ -21,7 +21,30 @@ const fs = require("fs");
 // console.log(data);
 
 // read file asyncrronously
-const data = fs.readFile("data/test.txt", "utf-8", (e, data) => {
-  if (e) throw err;
-  console.log(data);
+// const data = fs.readFile("data/test.txt", "utf-8", (e, data) => {
+//   if (e) throw err;
+//   console.log(data);
+// });
+// Readline
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Please input your name : ", (name) => {
+  rl.question("Please input your phone number : ", (phone) => {
+    const contact = { name, phone };
+    const file = fs.readFileSync("data/contacts.json", "utf-8");
+    const contacts = JSON.parse(file);
+
+    contacts.push(contact);
+
+    fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+
+    console.log("Thank you for your input.");
+
+    rl.close();
+  });
 });
