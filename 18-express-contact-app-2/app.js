cbec3a1feeaf553711db7fd0cd379ce5d31d9865
amri <amriroxadi@gmail.com>
2022-02-25 06:46:04 +0800

    express contact app part 2 till 9:39

---
+++ 18-express-contact-app-2/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+const { loadContact, findContact } = require("./utils/contacts");
+
+const port = 3000;
+
+app.set("view engine", "ejs"); // use ejs
+app.use(expressLayouts); // Third-party Middleware
+app.use(express.static("public")); //Build-in middleware
+
+app.get("/", (req, res) => {
+  const inhabitant = [
+    {
+      name: "Amri Rosadi",
+      email: "amriroxadi@gmail.com",
+    },
+    {
+      name: "Islah",
+      email: "islah@gmail.com",
+    },
+    {
+      name: "Erfan",
+      email: "erfan@gmail.com",
+    },
+  ];
+  res.render("index", {
+    name: "Amri Rosadi",
+    title: "Home Page",
+    inhabitant,
+    layout: "layouts/main-layout",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    layout: "layouts/main-layout",
+    title: "About Page",
+  });
+});
+
+app.get("/contact", (req, res) => {
+  const contacts = loadContact();
+  res.render("contact", {
+    layout: "layouts/main-layout",
+    title: "Contact Page",
+    contacts,
+  });
+});
+
+// Halaman form tambah data contact
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    title: "Add Contact Form",
+    layout: "layouts/main-layout",
+  });
+});
+
+// Halaman detail contact
+app.get("/contact/:name", (req, res) => {
+  const contact = findContact(req.params.name);
+  res.render("detail", {
+    layout: "layouts/main-layout",
+    title: "Details Contact Page",
+    contact,
+  });
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
